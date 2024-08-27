1a5e430be71fcf5d905ef3c23f89499a7f10da41
Lucas Axberg <lucas.axberg@gmail.com>
2024-08-27 17:39:01 +0200

    Added comments to improve readability

--- Scripts/CameraMovement.gd
+++ Scripts/CameraMovement.gd
@@ -1,32 +1,41 @@
 extends Camera3D
 
+# Set rotation speed and initialize rotation values
 var mouse_sensitivity := 0.001
 var twist_input := 0.0
 var pitch_input := 0.0
-@onready var pitch_pivot: Node3D = $".."
-@onready var player: RigidBody3D = $"../.."
+
+# Acess the nodes that will be rotated
+@onready var pitch_pivot: Node3D = $".."      # 'HeadPoint' node
+@onready var player: RigidBody3D = $"../.."   # 'Player' node
 
 
 func _ready() -> void:
+	# Hide and intercept mouse when game is started
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 func _physics_process(delta: float) -> void:
+	# Make mouse visible when ESC is pressed (PLACEHOLDER)
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
+	# Rotate the player along the y-axis and the "head" along the x-axis
 	player.rotate_y(twist_input)
 	pitch_pivot.rotate_x(pitch_input)
-	pitch_pivot.rotation.x = clamp(
-		pitch_pivot.rotation.x,
-		-0.5,
-		0.5,
-	)
+	
+	# Clamp rotation to prevent upside-down camera
+	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, -0.5, 0.5)
+	
+	# Reset rotation values so the camera stops spinning
 	twist_input = 0.0
 	pitch_input = 0.0
 
 func _unhandled_input(event: InputEvent) -> void:
+	# Asign twist- and pitch-input when mouse is captured and hidden 
 	if event is InputEventMouseMotion:
-		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:			
+			
+			# Use negative values so camera moves up when mouse does
 			twist_input = - event.relative.x * mouse_sensitivity
 			pitch_input = - event.relative.y * mouse_sensitivity
 			
--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -1,5 +1,6 @@
 extends RigidBody3D
 
 func _physics_process(delta: float) -> void:
+	# Apply a force along the players axis if a key is pressed
 	apply_central_force(Input.get_axis("move_left", "move_right") * basis.x * 1200.0 * delta)
 	apply_central_force(Input.get_axis("move_forward", "move_back") * basis.z * 1200.0 * delta)
