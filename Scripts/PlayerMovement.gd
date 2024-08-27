4a28e6887229b967da3ce0bb4e243b3515d927fc
Lucas Axberg <lucas.axberg@gmail.com>
2024-08-27 09:29:25 +0200

    Added movement script to player character

---
+++ Scripts/PlayerMovement.gd
@@ -0,0 +1,8 @@
+extends RigidBody3D
+
+func _process(delta: float) -> void:
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_back")
+	
+	apply_central_force(input * 1200.0 * delta)
