81bd5f47eb52535776184ff1663975682b1ccf0d
Lucas Axberg <lucas.axberg@gmail.com>
2024-09-03 09:03:15 +0200

    added target node and set enemy to move to target

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -2,21 +2,25 @@ extends CharacterBody3D
 
 const SPEED = 4.0
 
-# Initialize player variables
-var player = null
-@export var player_path: NodePath
 
-# Get navigation agent
+var player = null
+var target = null
+
+@export var player_path: NodePath
+@export var target_path: NodePath
+
+
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
 
 func _ready() -> void:
 	player = get_node(player_path)
+	target = get_node(target_path)
 	
 func _process(delta: float) -> void:
 	velocity = Vector3.ZERO
 	
 	# Navigation
-	navigation_agent_3d.set_target_position(player.global_transform.origin)
+	navigation_agent_3d.set_target_position(target.global_transform.origin)
 	var next_position = navigation_agent_3d.get_next_path_position()
 	velocity = (next_position - global_transform.origin)
 	move_and_slide()
