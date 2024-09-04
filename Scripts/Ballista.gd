3591654f5b41b0994cbefed2893a6f42e9cabbea
Lucas Axberg <lucas.axberg@gmail.com>
2024-09-04 10:18:34 +0200

    feature: detecting player via collision with vision cone and raycast

--- Scripts/Ballista.gd
+++ Scripts/Ballista.gd
@@ -1,12 +1,26 @@
 extends Node3D
 
 @onready var vision_area: Area3D = $VisionArea
+@onready var vision_raycast: RayCast3D = $VisionRaycast
 
 
 func _process(delta: float) -> void:
 	var overlaps = vision_area.get_overlapping_bodies()
 	if overlaps.size() > 0:
+		
 		for overlap in overlaps:
 			if overlap.is_in_group("enemy"):
-				# Code here
-				pass
+				var enemy_position = overlap.global_position
+				vision_raycast.look_at(enemy_position)
+				vision_raycast.force_raycast_update()
+				
+				if vision_raycast.is_colliding():
+					var collider = vision_raycast.get_collider()
+					if collider.is_in_group("enemy"):
+						vision_raycast.debug_shape_custom_color = Color(255, 0, 0)
+						print("I see you")
+					else:
+						vision_raycast.debug_shape_custom_color = Color(0, 255, 0)
+						print("I don't see you")
+						
+						
