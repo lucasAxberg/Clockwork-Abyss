2fb7fb7c508ce5e110f25c74985e6552379816b5
Lucas Axberg <lucas.axberg@gmail.com>
2024-09-10 11:05:22 +0200

    feature: attack timer for controlling attack speed

--- Scripts/Ballista.gd
+++ Scripts/Ballista.gd
@@ -9,19 +9,23 @@ var ammo = preload("res://Scenes/BalistaAmmo.tscn")
 
 @onready var shoot_point: Node3D = $ShootPoint
 
+@onready var attack_timer: Timer = $AttackTimer
+
 
 var ammo_list = []
 var direction_list = []
 var speed = 10
 
+
 func _process(delta: float) -> void:
 	var closest_enemy: Node3D = turret_functions.get_closest_enemy(vision_area, vision_raycast)
 	if closest_enemy:
-		if ammo_list.size() == 0:
+		if attack_timer.get_time_left() == 0:
 			ammo_list.append(ammo.instantiate())
 			direction_list.append(shoot_point.global_position.direction_to(closest_enemy.global_position))
-			add_child(ammo_list[0])
-			ammo_list[0].global_position = shoot_point.global_position
+			add_child(ammo_list[-1])
+			ammo_list[-1].global_position = shoot_point.global_position
+			attack_timer.start()
 		
 	turret_functions.move_ammo(ammo_list, direction_list, speed * delta)
 	#for a in range(ammo_list.size()):
