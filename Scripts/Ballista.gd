c4c58eb33126f4cb3dc3dbdb198339fe0b716a57
Lucas Axberg <lucas.axberg@gmail.com>
2024-09-10 08:56:00 +0200

    feature: class with functions for turrets

--- Scripts/Ballista.gd
+++ Scripts/Ballista.gd
@@ -1,26 +1,12 @@
 extends Node3D
 
+var turret_functions = Turret.new()
+
 @onready var vision_area: Area3D = $VisionArea
 @onready var vision_raycast: RayCast3D = $VisionRaycast
 
 
 func _process(delta: float) -> void:
-	var overlaps = vision_area.get_overlapping_bodies()
-	if overlaps.size() > 0:
-		
-		for overlap in overlaps:
-			if overlap.is_in_group("enemy"):
-				var enemy_position = overlap.global_position
-				vision_raycast.look_at(enemy_position)
-				vision_raycast.force_raycast_update()
-				
-				if vision_raycast.is_colliding():
-					var collider = vision_raycast.get_collider()
-					if collider.is_in_group("enemy"):
-						vision_raycast.debug_shape_custom_color = Color(255, 0, 0)
-						print("I see you")
-					else:
-						vision_raycast.debug_shape_custom_color = Color(0, 255, 0)
-						print("I don't see you")
-						
-						
+	var closest_enemy: Node3D = turret_functions.get_closest_enemy(vision_area, vision_raycast)
+	print(closest_enemy.name)
+	
---
+++ Scripts/TurretFunction.gd
@@ -0,0 +1,56 @@
+class_name Turret
+
+
+func get_closest_enemy(vision_area: Area3D, vision_raycast: RayCast3D) -> Node3D:
+	var overlaps = vision_area.get_overlapping_bodies()
+	
+	# Exit if no overlaps exist
+	if overlaps.size() <= 0:
+		return null
+	
+	# Initialize variables for closest node
+	var closest_node: Node3D
+	var closest_node_distance: float
+	
+	for overlap in overlaps:
+		
+		# Look at enemies and force raycast update
+		if overlap.is_in_group("enemy"):
+			var enemy_position = overlap.global_position
+			vision_raycast.look_at(enemy_position)
+			vision_raycast.force_raycast_update()
+			
+			# Check if raycast collides with a collider that is in enemy group
+			if vision_raycast.is_colliding():
+				var collider = vision_raycast.get_collider()
+				if collider.is_in_group("enemy"):
+					
+					# Check if new collider is closer than the previous one and update variables if nessecary
+					var distance: float = vision_raycast.global_position.distance_to(collider.global_position)
+					if distance > closest_node_distance:
+						closest_node_distance = distance
+						closest_node = collider
+	
+	return closest_node
+
+
+
+#var overlaps = vision_area.get_overlapping_bodies()
+	#if overlaps.size() > 0:
+		#
+		#for overlap in overlaps:
+			#if overlap.is_in_group("enemy"):
+				#var enemy_position = overlap.global_position
+				#vision_raycast.look_at(enemy_position)
+				#vision_raycast.force_raycast_update()
+				#
+				#if vision_raycast.is_colliding():
+					#var collider = vision_raycast.get_collider()
+					#if collider.is_in_group("enemy"):
+						#vision_raycast.debug_shape_custom_color = Color(255, 0, 0)
+						#print("I see you")
+					#else:
+						#vision_raycast.debug_shape_custom_color = Color(0, 255, 0)
+						#print("I don't see you")
+						#
+						#
