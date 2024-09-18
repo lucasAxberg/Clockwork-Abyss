55366c1fc08135404417d655926419e042ab1148
Lucas Axberg <lucas.axberg@gmail.com>
2024-09-18 08:40:09 +0200

    edit: bullet dissapears on collision with wall + wall group added. note: wall group must be added to StaticBody3D

--- Scripts/Classes/Bullet.gd
+++ Scripts/Classes/Bullet.gd
@@ -6,9 +6,11 @@ var _direction: Vector3
 var _speed: int
 var _bullet = null
 var _piercing: int
+
 # Variable to show if "_bullet" exists
 var exists: bool
 
+# Variables for collission detection
 var colliders_id = []
 var previous_pos: Vector3
 
@@ -20,31 +22,45 @@ func _init(pos: Vector3, dir: Vector3, speed: int, piercing: int, scene):
 	_speed = speed
 	_piercing = piercing
 	exists = true
+	
 	# Move bullet in <turret> scene to ShootingPoint's position
 	_bullet.global_position = _position
 	
 func move_ammo(delta) -> void:
 	# Save previous position
 	previous_pos = _bullet.global_position
+	
 	# Move bullet according to speed and direction
 	_bullet.global_position.x += _direction.x * _speed * delta
 	_bullet.global_position.z += _direction.z * _speed * delta
+	
 	# Get all colliders the bullet touches
-	#var colliders = _bullet.get_node("Area3D").get_overlapping_bodies()
 	var colliders = get_passed_colliders() + _bullet.get_node("Area3D").get_overlapping_bodies()
 	if colliders.size() > 0:
 		for collider in colliders:
-			if !collider.is_in_group("test"):
+			
+			# Checks if collider isn't a wall
+			if !collider.is_in_group("wall"):
+				
 				# Check if bullet hasnt allready collided with the collider
 				if !colliders_id.has(collider.get_instance_id()):
+					
+					# Remove bullet scene if bullet collides with a certain group and piercing = 0
 					if _piercing <= 0:
-						# Remove bullet scene if bullet collides with a certain group and piercing = 0
 						_bullet.queue_free()
 						exists = false
 						print("godbye")
+					
+					# Else add collider to list and decrease piercing
 					else:
 						colliders_id.append(collider.get_instance_id())
 						_piercing -= 1
+			
+			# Removes the bullet instantly if it collided with a wall
+			else:
+				_bullet.queue_free()
+				exists = false
+				print("godbye")
 
 func get_passed_colliders():
 	# Get all children of _bullet instance and filter to get the RayCast3D
