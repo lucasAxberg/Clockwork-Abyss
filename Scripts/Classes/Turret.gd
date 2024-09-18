7a8443b4915f8a451df540b1c8ec0aef6db8d9a3
Lucas Axberg <lucas.axberg@gmail.com>
2024-09-18 09:29:47 +0200

    feature: class for tracking stats of turrets

---
+++ Scripts/Classes/Turret.gd
@@ -0,0 +1,13 @@
+class_name Turret
+
+var _ammo_count: int
+var _max_ammo: int
+var _weapon_condition: float
+var _firerate: float
+var _damage: float
+
+var _turret_instance = null
+
+# Deal damage function
+func deal_damage(target):
+	pass
